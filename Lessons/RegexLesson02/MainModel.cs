68708b511d336bdf38c64ee05db55dc624b898db
Teacher Peng <pengsw@mail.buct.edu.cn>
2017-12-20 12:14:03 +0800

    Continue RegexLesson2.

--- Lessons/RegexLesson02/MainModel.cs
+++ Lessons/RegexLesson02/MainModel.cs
@@ -1,14 +1,52 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace RegexLesson02
 {
     class MainModel : INotifyPropertyChanged
     {
-        public string[] SourceTexts { get { return _SourceTexts; } set { if (_SourceTexts == value) return; _SourceTexts = value; OnPropertyChanged(nameof(SourceTexts)); } }
+        public void Load(string aFileName)
+        {
+            string[] aLines = File.ReadAllLines(aFileName);
+            SourceTexts = aLines;
+        }
+
+        public void DoFilter()
+        {
+            if (string.IsNullOrEmpty(Pattern))
+            {
+                FilteredTexts = new List<string>(SourceTexts);
+            }
+            else
+            {
+                Regex aRegex = new Regex(Pattern);
+                List<string> aLines = new List<string>();
+                foreach (string aLine in SourceTexts)
+                {
+                    if (aRegex.IsMatch(aLine))
+                        aLines.Add(aLine);
+                }
+                FilteredTexts = aLines;
+            }
+        }
+
+        public string[] SourceTexts
+        {
+            get { return _SourceTexts; }
+            set
+            {
+                if (_SourceTexts == value) return;
+                _SourceTexts = value;
+                DoFilter();
+                OnPropertyChanged(nameof(SourceTexts));
+            }
+        }
         private string[] _SourceTexts;
 
-        public string[] FilterdTexts { get { return _FilterdTexts; } set { if (_FilterdTexts == value) return; _FilterdTexts = value; OnPropertyChanged(nameof(FilterdTexts)); } }
-        private string[] _FilterdTexts;
+        public List<string> FilteredTexts { get { return _FilterdTexts; } private set { if (_FilterdTexts == value) return; _FilterdTexts = value; OnPropertyChanged(nameof(FilteredTexts)); } }
+        private List<string> _FilterdTexts;
 
         public string Pattern { get { return _Pattern; } set { if (_Pattern == value) return; _Pattern = value; OnPropertyChanged(nameof(Pattern)); } }
         private string _Pattern;
--- Lessons/RegexLesson02/MainWindow.xaml.cs
+++ Lessons/RegexLesson02/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Microsoft.Win32;
 
 namespace RegexLesson02
 {
@@ -14,5 +15,27 @@ namespace RegexLesson02
             this.DataContext = _Model;
         }
         private MainModel _Model;
+
+        private void OnLoad_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog aDlg = new OpenFileDialog();
+            if (aDlg.ShowDialog() != true) return;
+            _Model.Load(aDlg.FileName);
+        }
+
+        private void OnLoad_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnStartFilter_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            _Model.DoFilter();
+        }
+
+        private void OnStartFilter_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
     }
 }
