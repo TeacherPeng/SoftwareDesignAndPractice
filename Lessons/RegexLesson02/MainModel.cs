eb7f211953dab3ca17082bc5006361d394341b89
Teacher Peng <pengsw@mail.buct.edu.cn>
2017-12-20 11:34:24 +0800

    修正Filter的拼写错误

--- Lessons/RegexLesson02/MainModel.cs
+++ Lessons/RegexLesson02/MainModel.cs
@@ -7,8 +7,8 @@ namespace RegexLesson02
         public string[] SourceTexts { get { return _SourceTexts; } set { if (_SourceTexts == value) return; _SourceTexts = value; OnPropertyChanged(nameof(SourceTexts)); } }
         private string[] _SourceTexts;
 
-        public string[] FiltedTexts { get { return _FiltedTexts; } set { if (_FiltedTexts == value) return; _FiltedTexts = value; OnPropertyChanged(nameof(FiltedTexts)); } }
-        private string[] _FiltedTexts;
+        public string[] FilterdTexts { get { return _FilterdTexts; } set { if (_FilterdTexts == value) return; _FilterdTexts = value; OnPropertyChanged(nameof(FilterdTexts)); } }
+        private string[] _FilterdTexts;
 
         public string Pattern { get { return _Pattern; } set { if (_Pattern == value) return; _Pattern = value; OnPropertyChanged(nameof(Pattern)); } }
         private string _Pattern;
