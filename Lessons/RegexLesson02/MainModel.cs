2560167f5fa1af317e391c7d17535bd9d2acae1c
Teacher Peng <pengsw@mail.buct.edu.cn>
2017-12-20 11:31:00 +0800

    Create RegexLesson02.

---
+++ Lessons/RegexLesson02/MainModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace RegexLesson02
+{
+    class MainModel : INotifyPropertyChanged
+    {
+        public string[] SourceTexts { get { return _SourceTexts; } set { if (_SourceTexts == value) return; _SourceTexts = value; OnPropertyChanged(nameof(SourceTexts)); } }
+        private string[] _SourceTexts;
+
+        public string[] FiltedTexts { get { return _FiltedTexts; } set { if (_FiltedTexts == value) return; _FiltedTexts = value; OnPropertyChanged(nameof(FiltedTexts)); } }
+        private string[] _FiltedTexts;
+
+        public string Pattern { get { return _Pattern; } set { if (_Pattern == value) return; _Pattern = value; OnPropertyChanged(nameof(Pattern)); } }
+        private string _Pattern;
+
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Lessons/RegexLesson02/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace RegexLesson02
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MainModel();
+            this.DataContext = _Model;
+        }
+        private MainModel _Model;
+    }
+}
