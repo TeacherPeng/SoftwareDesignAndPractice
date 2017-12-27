20ccb8d2d1e198e57e3e3b1238eef84d5617fcab
Teacher Peng <pengsw@mail.buct.edu.cn>
2017-12-27 10:35:14 +0800

    Create RegexLesson03

---
+++ Lessons/RegexLesson03/MainModel.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexLesson03
+{
+    class MainModel : INotifyPropertyChanged
+    {
+        public string Pattern
+        {
+            get { return _Pattern; }
+            set
+            {
+                if (_Pattern == value) return; _Pattern = value; OnPropertyChanged(nameof(Pattern));
+            }
+        }
+        private string _Pattern;
+
+        public string ReplacePattern { get { return _ReplacePattern; } set { if (_ReplacePattern == value) return; _ReplacePattern = value; OnPropertyChanged(nameof(ReplacePattern)); } }
+        private string _ReplacePattern;
+
+        public string TargetText { get { return _TargetText; } set { if (_TargetText == value) return; _TargetText = value; OnPropertyChanged(nameof(TargetText)); } }
+        private string _TargetText;
+
+        public MatchCollection Matches { get { return _Matches; } private set { if (_Matches == value) return; _Matches = value; OnPropertyChanged(nameof(Matches)); } }
+        private MatchCollection _Matches;
+
+        public string CollectedText { get { return _CollectedText; } private set { if (_CollectedText == value) return; _CollectedText = value; OnPropertyChanged(nameof(CollectedText)); } }
+        private string _CollectedText;
+
+        public string ReplaceResult { get { return _ReplaceResult; } private set { if (_ReplaceResult == value) return; _ReplaceResult = value; OnPropertyChanged(nameof(ReplaceResult)); } }
+        private string _ReplaceResult;
+
+        public bool CanStartMatch
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Pattern) && !string.IsNullOrWhiteSpace(TargetText);
+            }
+        }
+
+        public bool CanStartReplace
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Pattern) && !string.IsNullOrWhiteSpace(ReplacePattern) && !string.IsNullOrWhiteSpace(TargetText);
+            }
+        }
+
+        public void LoadTargetTextFrom(string aFileName)
+        {
+            TargetText = File.ReadAllText(aFileName);
+        }
+
+        public void GetMatches()
+        {
+            Regex aRegex = new Regex(Pattern);
+            Matches = aRegex.Matches(TargetText);
+            StringBuilder aStringBuilder = new StringBuilder();
+            foreach (Match aMatch in Matches)
+            {
+                aStringBuilder.AppendLine(aMatch.Value);
+            }
+            CollectedText = aStringBuilder.ToString();
+        }
+
+        public void Replace()
+        {
+            Regex aRegex = new Regex(Pattern);
+            ReplaceResult = aRegex.Replace(TargetText, ReplacePattern);
+        }
+
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Lessons/RegexLesson03/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace RegexLesson03
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MainModel();
+            this.DataContext = _Model;
+        }
+        private MainModel _Model;
+
+        private void OnLoadTargetText_Exedcuted(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog aDlg = new OpenFileDialog();
+                aDlg.Filter = "文本文件|*.txt;*.html;*.log|所有文件|*.*";
+                if (aDlg.ShowDialog() != true) return;
+                _Model.LoadTargetTextFrom(aDlg.FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnLoadTargetText_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnStartMatch_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                _Model.GetMatches();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnStartMatch_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null && _Model.CanStartMatch;
+        }
+
+        private void OnStartReplace_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                _Model.Replace();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnStartReplace_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null && _Model.CanStartReplace;
+        }
+    }
+}
