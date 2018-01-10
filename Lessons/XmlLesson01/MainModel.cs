6d8ccb1521e376a5c6edd0e69de7d6145c8e7e07
Teacher Peng <pengsw@mail.buct.edu.cn>
2018-01-10 11:17:46 +0800

    Create Xml Lesson 01

---
+++ Lessons/XmlLesson01/MainModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XmlLesson01
+{
+    class MainModel : INotifyPropertyChanged
+    {
+        public XmlDocument CurrentXml { get { return _CurrentXml; } set { if (_CurrentXml == value) return; _CurrentXml = value; OnPropertyChanged(nameof(CurrentXml)); } }
+        private XmlDocument _CurrentXml;
+        private string _FileName;
+
+        public void LoadXmlFile(string aFileName)
+        {
+            XmlDocument aXml = new XmlDocument();
+            aXml.Load(aFileName);
+            CurrentXml = aXml;
+            _FileName = aFileName;
+        }
+        private const string ConfigFileName = "xmllesson01.cfg";
+        public void SaveConfig()
+        {
+            XDocument aXDocument = new XDocument(
+                new XElement("Config", 
+                    new XElement("LastFile", _FileName)
+                )
+            );
+            aXDocument.Save(ConfigFileName);
+        }
+        public void LoadConfig()
+        {
+            XDocument aXDocument = XDocument.Load(ConfigFileName);
+            _FileName = aXDocument.Root.Element("LastFile").Value;
+            LoadXmlFile(_FileName);
+        }
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Lessons/XmlLesson01/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace XmlLesson01
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                _Model = new MainModel();
+                _Model.LoadConfig();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            this.DataContext = _Model;
+        }
+        private MainModel _Model;
+
+        private void OnOpen_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog aDlg = new OpenFileDialog();
+                aDlg.Filter = "Xml 文件 (*.xml)|*.xml|所有文件 (*.*)|*.*";
+                if (aDlg.ShowDialog() != true) return;
+                _Model.LoadXmlFile(aDlg.FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            try
+            {
+                _Model.SaveConfig();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
