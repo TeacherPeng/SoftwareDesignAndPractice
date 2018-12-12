c1a78823b9f0dc0682bbc62c0829b2b0bcc0facb
Teacher Peng <pengsw@mail.buct.edu.cn>
2018-12-12 10:04:55 +0800

    更新CS008

--- Lessons/CS008/Drawer.cs
+++ Lessons/CS008/Drawer.cs
@@ -2,27 +2,23 @@
 {
     class Drawer
     {
-        public Drawer(int w, int h, double x0, double y0, double x1, double y1)
+        public Drawer(double x0, double y0, double x1, double y1)
         {
-            W = w;
-            H = h;
             X0 = x0;
             Y0 = y0;
             X1 = x1;
             Y1 = y1;
         }
-        public int W { get; set; }
-        public int H { get; set; }
         public double X0 { get; set; }
         public double Y0 { get; set; }
         public double X1 { get; set; }
         public double Y1 { get; set; }
 
         public delegate double DrawDelegate(double x);
-        public int[,] Draw(DrawDelegate f)
+        public void Draw(int[,] aImage, DrawDelegate f)
         {
-            int[,] aImage = new int[H, W];
-
+            int W = aImage.GetLength(1);
+            int H = aImage.GetLength(0);
             for (int j = 0; j < W; j++)
             {
                 double x = j * (X1 - X0) / (W - 1) + X0;
@@ -30,7 +26,6 @@
                 int i = (int)((H - 1) - (y - Y0) / (Y1 - Y0) * (H - 1));
                 if (i >= 0 && i < H) aImage[i, j] = 1;
             }
-            return aImage;
         }
     }
 }
--- Lessons/CS008/Program.cs
+++ Lessons/CS008/Program.cs
@@ -6,23 +6,26 @@ namespace CS008
     {
         static double MyFunc(double x)
         {
-            return Math.Sin(x) + Math.Cos(x);
+            return Math.Cos(x);
         }
         static void Main(string[] args)
         {
-            Drawer aDrawer = new Drawer(80, 25, 0, -1, 3.14*2, 1);
-            int[,] aImage = aDrawer.Draw(Math.Sin);
-            DrawOnConsole(aDrawer, aImage);
-            int[,] aImage2 = aDrawer.Draw(MyFunc);
-            DrawOnConsole(aDrawer, aImage2);
+            int[,] aImage = new int[25, 80];
+            Drawer aDrawer = new Drawer(0, -1, 3.14*2, 1);
+            aDrawer.Draw(aImage, Math.Sin);
+            DrawOnConsole(aImage);
+            aDrawer.Draw(aImage, MyFunc);
+            DrawOnConsole(aImage);
             Console.ReadLine();
         }
 
-        private static void DrawOnConsole(Drawer aDrawer, int[,] aImage)
+        private static void DrawOnConsole(int[,] aImage)
         {
-            for (int i = 0; i < aDrawer.H; i++)
+            int W = aImage.GetLength(1);
+            int H = aImage.GetLength(0);
+            for (int i = 0; i < H; i++)
             {
-                for (int j = 0; j < aDrawer.W; j++)
+                for (int j = 0; j < W; j++)
                 {
                     Console.Write(aImage[i, j] == 1 ? "*" : " ");
                 }
