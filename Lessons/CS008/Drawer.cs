d4fa0cb7276eadb7b4958b2bc7c4467204bf6780
Teacher Peng <pengsw@mail.buct.edu.cn>
2018-12-12 09:43:34 +0800

    添加CS008

---
+++ Lessons/CS008/Drawer.cs
@@ -0,0 +1,36 @@
+﻿namespace CS008
+{
+    class Drawer
+    {
+        public Drawer(int w, int h, double x0, double y0, double x1, double y1)
+        {
+            W = w;
+            H = h;
+            X0 = x0;
+            Y0 = y0;
+            X1 = x1;
+            Y1 = y1;
+        }
+        public int W { get; set; }
+        public int H { get; set; }
+        public double X0 { get; set; }
+        public double Y0 { get; set; }
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+
+        public delegate double DrawDelegate(double x);
+        public int[,] Draw(DrawDelegate f)
+        {
+            int[,] aImage = new int[H, W];
+
+            for (int j = 0; j < W; j++)
+            {
+                double x = j * (X1 - X0) / (W - 1) + X0;
+                double y = f(x);
+                int i = (int)((H - 1) - (y - Y0) / (Y1 - Y0) * (H - 1));
+                if (i >= 0 && i < H) aImage[i, j] = 1;
+            }
+            return aImage;
+        }
+    }
+}
---
+++ Lessons/CS008/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CS008
+{
+    class Program
+    {
+        static double MyFunc(double x)
+        {
+            return Math.Sin(x) + Math.Cos(x);
+        }
+        static void Main(string[] args)
+        {
+            Drawer aDrawer = new Drawer(80, 25, 0, -1, 3.14*2, 1);
+            int[,] aImage = aDrawer.Draw(Math.Sin);
+            DrawOnConsole(aDrawer, aImage);
+            int[,] aImage2 = aDrawer.Draw(MyFunc);
+            DrawOnConsole(aDrawer, aImage2);
+            Console.ReadLine();
+        }
+
+        private static void DrawOnConsole(Drawer aDrawer, int[,] aImage)
+        {
+            for (int i = 0; i < aDrawer.H; i++)
+            {
+                for (int j = 0; j < aDrawer.W; j++)
+                {
+                    Console.Write(aImage[i, j] == 1 ? "*" : " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
